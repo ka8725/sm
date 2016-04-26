4569c22f4228d835ab4ebc30d69e551db60ba57c
Andrey Koleshko <ka8725@gmail.com>
2016-04-26 15:44:22 +0300

    Start coding

---
+++ lib/sm.rb
@@ -0,0 +1,89 @@
+require 'sm/version'
+require 'set'
+
+module StateMachine
+  def self.included(base)
+    class << base
+      attr_accessor :initial_state
+
+      def state(name, initial: false)
+        state_value_must_be_safe!(name)
+        add_state(name)
+        assign_initial_state(name) if initial
+      end
+
+      def defined_state?(name)
+        states.include?(name)
+      end
+
+      def state_value_must_be_safe!(name)
+        return if name.is_a?(Symbol)
+
+        fail ArgumentError, "State must be a Symbol. Given #{name.class}: #{name}"
+      end
+
+      def state_must_be_defined!(name)
+        return if states.include?(name)
+
+        fail ArgumentError, "State :#{name} is undefined"
+      end
+
+      private
+
+      def add_state(name)
+        state_must_not_be_defined!(name)
+
+        states.add(name)
+      end
+
+      def states
+        @states ||= Set.new
+      end
+
+      def assign_initial_state(name)
+        initial_state_must_not_be_set!
+
+        self.initial_state = name
+      end
+
+      def initial_state_must_not_be_set!
+        return unless initial_state
+
+        fail ArgumentError, "Initial state is already set to #{initial_state}"
+      end
+
+      def state_must_not_be_defined!(name)
+        return unless states.include?(name)
+
+        fail ArgumentError, "State :#{name} is already defined"
+      end
+    end
+  end
+
+  attr_reader :state
+
+  def initialize(initial_state = nil)
+    @state = set_state(initial_state || statically_defined_initial_state)
+  end
+
+  private
+
+  def set_state(name)
+    state_value_must_be_safe!(name)
+    state_must_be_defined!(name)
+
+    @state = name
+  end
+
+  def state_value_must_be_safe!(name)
+    self.class.state_value_must_be_safe!(name)
+  end
+
+  def state_must_be_defined!(name)
+    self.class.state_must_be_defined!(name)
+  end
+
+  def statically_defined_initial_state
+    self.class.initial_state
+  end
+end
---
+++ test/sm_test.rb
@@ -0,0 +1,64 @@
+require 'minitest/autorun'
+require 'test_helper'
+
+class MovementState
+  include StateMachine
+
+  state :standing, initial: true
+  state :walking
+end
+
+describe StateMachine do
+  describe 'DSL' do
+    def error_message_on_state_definition(&def_dsl)
+      message = nil
+      begin
+        Class.new do
+          include StateMachine
+
+          instance_eval(&def_dsl)
+        end
+      rescue => e
+        message = e.message
+      end
+
+      message
+    end
+
+    describe '.state' do
+      it 'does not allow to define initial state twice' do
+        assert_equal('Initial state is already set to standing',
+          error_message_on_state_definition do
+            state :standing, initial: true
+            state :walking, initial: true
+          end)
+      end
+
+      it 'does not allow to define the same state twice' do
+        assert_equal('State :standing is already defined',
+          error_message_on_state_definition do
+            state :standing
+            state :standing
+          end)
+      end
+
+      it 'does not allow to define state with strings' do
+        assert_equal('State must be a Symbol. Given String: standing',
+          error_message_on_state_definition do
+            state 'standing'
+          end)
+      end
+    end
+  end
+
+  describe '#new' do
+    it 'allows to assigns initial state manually' do
+      assert_equal :standing, MovementState.new.state
+    end
+
+    it 'should set initial state set to statically defined one' do
+      assert_equal :walking, MovementState.new(:walking).state
+    end
+  end
+end
+
